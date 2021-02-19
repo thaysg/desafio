febdb5c36cf32cb7532d7decac4358ea61824096
thaysg <thay.a.garcia@gmail.com>
2021-02-19 20:52:28 -0300

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function loadJSON(callback) {   
+
+
+    alert("Hello");
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'dados.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
+ function init() {
+    loadJSON(function(response) {
+     // Parse JSON string into object
+       var actual_JSON = JSON.parse(response);
+    });
+   }
